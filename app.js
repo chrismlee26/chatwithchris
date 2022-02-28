c2b160de8d64886348e1ad8d044626a98fe4b4de
CHRIS_LEE <chrismlee87@gmail.com>
2022-02-27 19:42:08 -0800

    [add] initialize sockets.io

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const express = require('express')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io')
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server)
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+io.on('connection', socket => {
+  console.log('Websocket Connected.')
+})
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));

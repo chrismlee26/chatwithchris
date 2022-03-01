b8652e85d9296a8b7a3d4f5539aa1cb9ca87c221
CHRIS_LEE <chrismlee87@gmail.com>
2022-03-01 00:44:32 -0800

    [add] scroll to new message and chat input reset after submit

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,21 @@
 const chatForm = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-messages')
 
 const socket = io();
 
 // Message from Server
 socket.on('message', message => {
   outputMessage(message)
-  // console.log(message)
+  chatMessages.scrollTop == chatMessages.scrollHeight // Scroll to newest
 })
 
 // Submit Message from Client
 chatForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const msg = e.target.elements.msg.value // Get Message Text
-  // Emit message to Server
-  socket.emit('chatMessage', msg)
+  socket.emit('chatMessage', msg) // Emit message to Server
+  e.target.elements.msg.value = '' // Reset chat input after submit
+  e.target.elements.msg.focus();
 })
 
 // Output Message to DOM

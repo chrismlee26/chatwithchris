f42d046cc55c73c354dca8f38049afb8028da453
CHRIS_LEE <chrismlee87@gmail.com>
2022-03-01 00:30:54 -0800

    [add] Submit message from client to server

--- app.js
+++ app.js
@@ -9,8 +9,21 @@ const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'static')));
 
+// Run on client connection
 io.on('connection', socket => {
-  console.log('Websocket Connected.')
+  // User connected
+  socket.emit('message', 'You\'re chatting!')
+  // Broadcast when user connects
+  socket.broadcast.emit('message', 'Someone has joined the chat');
+  // Run on client disconnection
+  socket.on('disconnect', () => {
+    io.emit('message', 'Someone left the chat')
+  })
+  // Chat message listener
+  socket.on('chatMessage', (chatMsg) => {
+    io.emit('message', chatMsg)
+  })
+
 })
 
 const PORT = 3000 || process.env.PORT;
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,28 @@
+const chatForm = document.getElementById('chat-form')
+
 const socket = io();
+
+// Message from Server
+socket.on('message', message => {
+  outputMessage(message)
+  // console.log(message)
+})
+
+// Submit Message from Client
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const msg = e.target.elements.msg.value // Get Message Text
+  // Emit message to Server
+  socket.emit('chatMessage', msg)
+})
+
+// Output Message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div')
+  div.classList.add('message');
+  div.innerHTML = `
+    <p class="meta">Username <span>TimeStamp</span></p>
+    <p class="text">${message}</p>
+  `;
+  document.querySelector('.chat-messages').appendChild(div);
+}

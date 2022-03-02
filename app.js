d48b658493a530d5670578ffc905591fc30d6b8a
CHRIS_LEE <chrismlee87@gmail.com>
2022-03-02 13:36:30 -0800

    [add] update username and chatroom variables to pass through  websockets

--- app.js
+++ app.js
@@ -1,33 +1,55 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io')
-const formatMessage = require('./utils/messages')
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, getRoomUsers } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'static')));
+
 botName = 'chrischatbot'
 
 // Run on client connection
 io.on('connection', socket => {
-  // User connected
-  socket.emit('message', formatMessage(botName, 'You\'re chatting!'))
-  // Broadcast when user connects
-  socket.broadcast.emit('message', formatMessage(botName, 'Someone has joined the chat'));
+  socket.on('joinRoom', ({ username, chatroom }) => {
+    const user = userJoin(socket.id, username, chatroom)
+
+    socket.join(user.chatroom)
+
+    // User connected
+    socket.emit('message', formatMessage(botName, 'You\'re chatting!'))
+
+    // Broadcast when user connects
+    socket.broadcast
+      .to(user.chatroom)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+    // Send users and room info
+    io.to(user.chatroom).emit('roomUsers', {
+      room: user.chatroom,
+      users: getRoomUsers(user.chatroom)
+    });
+  })
+
+
+  // Chat message listener
+  socket.on('chatMessage', (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.chatroom).emit('message', formatMessage(user.username, msg))
+  })
   // Run on client disconnection
   socket.on('disconnect', () => {
     io.emit('message', formatMessage(botName, 'Someone left the chat'))
   })
-  // Chat message listener
-  socket.on('chatMessage', (msg) => {
-    io.emit('message', formatMessage('USER', msg))
-  })
-
 })
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- utils/users.js
+++ utils/users.js
@@ -12,7 +12,13 @@ function getCurrentUser(id) {
   return users.find(user => user.id === id);
 }
 
+// Get room users
+function getRoomUsers(room) {
+  return users.filter(user => user.room === room);
+}
+
 module.exports = {
   userJoin,
   getCurrentUser,
+  getRoomUsers,
 }
